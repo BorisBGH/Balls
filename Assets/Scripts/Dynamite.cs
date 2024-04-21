7c4aefa8697776e64f7ecd6068cb4282ec3c074d
BorisBGH <bvboris87@gmail.com>
2024-04-21 23:26:26 +0300

    Dynamite can blow

--- Assets/Scripts/Dynamite.cs
+++ Assets/Scripts/Dynamite.cs
@@ -10,7 +10,7 @@ public class Dynamite : ActiveItem
     [SerializeField] private GameObject _affectArea;
     [SerializeField] private GameObject _effectPref;
 
-  
+
     protected override void Start()
     {
         base.Start();
@@ -29,6 +29,25 @@ public class Dynamite : ActiveItem
         _affectArea.SetActive(true);
         _animator.enabled = true;
         yield return new WaitForSeconds(1f);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _affectRadius);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody rigidbody = collider.attachedRigidbody;
+            if (rigidbody)
+            {
+                Vector3 fromTo = (rigidbody.transform.position - transform.position).normalized;
+                rigidbody.AddForce(fromTo * _forceValue + Vector3.up * _forceValue * 0.5f);
+
+                PassiveItem passiveItem = rigidbody.GetComponent<PassiveItem>();
+                if (passiveItem)
+                {
+                    passiveItem.OnAffect();
+                }
+            }
+
+        }
+
         Instantiate(_effectPref, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
